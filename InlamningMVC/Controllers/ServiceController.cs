347ca3bbc61250c70bc3fb3fa1aa15fae4cf1610
Mary Wikdahl <mary.wikdahl@utb.ecutbildning.se>
2022-03-26 18:36:02 +0100

    Inlamning 1 MVC

---
+++ InlamningMVC/Controllers/ServiceController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace InlamningMVC.Controllers
+{
+    public class ServiceController : Microsoft.AspNetCore.Mvc.Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
